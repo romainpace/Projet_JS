f2d6fdd8c8536fae29e08167435650d9899b8416
Unknown <kratzmarc@gmail.com>
2017-12-26 15:23:15 +0100

    add CSS

--- projet_JS.js
+++ projet_JS.js
@@ -6,3 +6,13 @@ window.onload = function() {
     ctx = canvas.getContext("2d");
     ctx.fillRect(20,20,150,100);
 }
+
+
+//terrain 
+//terrain pour le monstre 
+//terrain 
+
+//monstre 
+
+
+// batiment
